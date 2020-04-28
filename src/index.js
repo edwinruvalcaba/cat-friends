9b73f4f45d678d079e089d17ec846fdaa22a4932
Edwin Ruvalcaba <edwinruvalcaba@Edwins-MacBook-Pro.local>
2020-04-27 18:34:48 -0700

    created hello component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hello from './Hello.js';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Hello greeting={'Hello React Ninja'}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
