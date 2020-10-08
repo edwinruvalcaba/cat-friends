d383aec5a162c8de1ae4198c5c175d7aa396b1f4
Edwin Ruvalcaba <edwinruvalcaba@gmail.com>
2020-10-08 16:29:14 -0700

    Updates

---
+++ precache-manifest.3368ab6c90212d667d4347e7a88d0edd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "778579334d9fcde2dc6c9f55462bc124",
+    "url": "/cat-friends/index.html"
+  },
+  {
+    "revision": "939c13df5b8167a85c58",
+    "url": "/cat-friends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "73fbb6a4347df8780de7",
+    "url": "/cat-friends/static/css/main.ea969f37.chunk.css"
+  },
+  {
+    "revision": "939c13df5b8167a85c58",
+    "url": "/cat-friends/static/js/2.5b6aa8d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cat-friends/static/js/2.5b6aa8d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73fbb6a4347df8780de7",
+    "url": "/cat-friends/static/js/main.338d4c73.chunk.js"
+  },
+  {
+    "revision": "55d9fd4f0bb01cb6b27e",
+    "url": "/cat-friends/static/js/runtime-main.6474270c.js"
+  },
+  {
+    "revision": "f543783b21543591cd961ebcce56f706",
+    "url": "/cat-friends/static/media/KittenBoldTrial.f543783b.woff"
+  }
+]);
